303b3f1fee34f872a83fe3587a267c25a5526c77
Yee <yee.gamedev@gmail.com>
2022-05-20 16:51:17 -0500

    Attacker Now Waits for Inputs

--- Assets/Attacker.cs
+++ Assets/Attacker.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
+    [SerializeField] int totalCommands = 2;
+
     public void Attack()
     {
-        Debug.Log("Attack");
+        StartCoroutine(ReadQTE());
+    }
+
+    IEnumerator ReadQTE()
+    {
+        Debug.Log("Attack Starts!");
+
+        for (int i = 0; i < totalCommands; i++)
+        {
+            yield return new WaitUntil(() => Input.GetKeyUp(KeyCode.L));
+            Debug.Log("Hi!");
+            yield return new WaitForEndOfFrame();
+        }
+
+        Debug.Log("QTE Completed");
     }
 }
